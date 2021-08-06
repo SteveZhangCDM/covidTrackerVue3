d811f183450cac44b63733934b8bd9b1f3c65011
Steve_Zhang <wentaozhang0305@gmail.com>
2021-08-05 21:05:02 -0700

    initial commit

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="grid md:gridd-cols-2 gap-4">
+    <!-- Box 1  -->
+    <div class="shadow-mf bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">
+        Cases
+
+        <div class="text-2xl mb-4">
+          <span class="font-bold">New:</span>
+          {{ stats.NewConfirmed }}
+        </div>
+        <div class="text-2xl mb-4">
+          <span class="font-bold">Total:</span>
+          {{ stats.TotalConfirmed }}
+        </div>
+      </h3>
+    </div>
+    <!-- Box 2  -->
+    <div class="shadow-mf bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">
+        Cases
+
+        <div class="text-2xl mb-4">
+          <span class="font-bold">New:</span>
+          {{ stats.NewDeaths }}
+        </div>
+        <div class="text-2xl mb-4">
+          <span class="font-bold">Total:</span>
+          {{ stats.TotalDeaths }}
+        </div>
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataBoxes',
+  props: ['stats'],
+  methods: {},
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <data-title :text="text" :dataDate="dataDate" />
+    <data-boxes :stats="stats" />
+    <country-select :countries="countries" @get-country="getCountryData" />
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-grey-500 text-3xl mt-10 mb-6">FetchingData</div>
+    <img :src="loadingImg" class="w-24 m-auto" alt="" />
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import DataTitle from '@/components/DataTitle.vue';
+import DataBoxes from '@/components/DataBoxes.vue';
+import CountrySelect from '@/components/CountrySelect.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    DataTitle,
+    DataBoxes,
+    CountrySelect,
+  },
+  data() {
+    return {
+      loading: true,
+
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+
+      /* eslint-disable global-require */
+
+      loadingImg: require('../../public/loading.gif'),
+
+      /* eslint-enable global-require */
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
   },
 };
 </script>
